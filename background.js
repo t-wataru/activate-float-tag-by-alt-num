375b384b9852b6e8858200b25192106f44759001
Wataru Tsujimoto <tsujimoto.wataru@g.wakayama-u.jp>
2020-08-03 21:19:20 +0900

    shortcutkey move to command from pagescript

--- background.js
+++ background.js
@@ -1,21 +1,20 @@
-let debug = true;
+let debug = false;
 log = debug ? console.log.bind(null, "DEBUG: ") : ()=>{};
 
-browser.runtime.onMessage.addListener(async (message, sender) => {
-    log("got messsage", message);
-    log("sender", sender);
-    tabIndex = message.activateFloatTabIndex;
-    log(tabIndex);
-    if (tabIndex) {
-      pinnedTabs = await browser.tabs.query(
-        {pinned: true}
-      );
-      targetTabs = await browser.tabs.query(
-        {index: tabIndex+pinnedTabs.length-1}
-      );
+browser.commands.onCommand.addListener(async (command)=>{
+  log(command);
+  const commands = ["_select_1st_tab", "_select_2nd_tab", "_select_3rd_tab", "_select_4th_tab", "_select_5th_tab", "_select_6th_tab", "_select_7th_tab", "_select_8th_tab", "_select_9th_tab", "_select_10th_tab"];
+  const tabIndex = commands.indexOf(command);
+  log(tabIndex);
+  
+  pinnedTabs = await browser.tabs.query(
+    {pinned: true}
+  );
+  targetTabs = await browser.tabs.query(
+    {index: tabIndex+pinnedTabs.length}
+  );
 
-      browser.tabs.update(targetTabs[0].id, {
-        active: true
-      });
-    }
+  browser.tabs.update(targetTabs[0].id, {
+    active: true
+  });
 });
